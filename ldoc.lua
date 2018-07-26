1ebb87e0b3247fd0a6274338730fb78e41b62b71
Jordan Irwin <antumdeluge@gmail.com>
2018-07-25 17:42:36 -0700

    Enable using 'print' command from within 'config.ld'

--- ldoc.lua
+++ ldoc.lua
@@ -244,6 +244,9 @@ end
 -- Enable `require` command within `config.ld`
 ldoc.require = require
 
+-- Enable `print` command within `config.ld`
+ldoc.print = print
+
 local ldoc_contents = {
    'alias','add_language_extension','custom_tags','new_type','add_section', 'tparam_alias',
    'file','project','title','package', 'icon','format','output','dir','ext', 'topics',
@@ -257,7 +260,7 @@ local ldoc_contents = {
    'postprocess_html',
    'custom_css','version',
    'no_args_infer',
-   'require',
+   'require', 'print',
 }
 ldoc_contents = tablex.makeset(ldoc_contents)
 
