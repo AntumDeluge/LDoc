671e5024aaaeb3f388eb9e00ddfca1f4373c79d5
Jordan Irwin <antumdeluge@gmail.com>
2019-02-15 03:50:28 +0000

    Make type & quit functions visible to config.ld

--- ldoc.lua
+++ ldoc.lua
@@ -247,6 +247,8 @@ ldoc.print = print
 ldoc.table = table
 ldoc.tostring = tostring
 ldoc.string = string
+ldoc.type = type
+ldoc.quit = quit
 
 local ldoc_contents = {
    'alias','add_language_extension','custom_tags','new_type','add_section', 'tparam_alias',
@@ -261,7 +263,7 @@ local ldoc_contents = {
    'postprocess_html',
    'custom_css','version',
    'no_args_infer',
-   'require', 'print', 'table', 'tostring', 'string',
+   'require', 'print', 'table', 'tostring', 'string', 'type', 'quit',
 }
 ldoc_contents = tablex.makeset(ldoc_contents)
 
