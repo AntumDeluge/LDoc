d618064eace56870bd741a272de6d69add625011
Jordan Irwin <antumdeluge@gmail.com>
2019-03-05 18:54:21 -0800

    Make global tonumber function visible to config.ld

--- ldoc.lua
+++ ldoc.lua
@@ -245,6 +245,7 @@ ldoc.require = require
 ldoc.print = print
 ldoc.table = table
 ldoc.tostring = tostring
+ldoc.tonumber = tonumber
 ldoc.string = string
 ldoc.type = type
 ldoc.quit = quit
@@ -265,7 +266,7 @@ local ldoc_contents = {
    'custom_css','version',
    'no_args_infer',
    'require', 'print', 'table', 'tostring', 'string', 'type', 'quit', 'ldoc_version',
-   'io',
+   'io', 'tonumber',
 }
 ldoc_contents = tablex.makeset(ldoc_contents)
 
